16ad94dad11dd836f4e133cf4ed611c43f187f6c
tsalmon <th_s@hotmail.fr>
2014-10-25 17:18:25 +0200

    p01

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -2,6 +2,15 @@ package org.p99.scala
 
 object P01 {
 
-  // TODO
+	def last(l: List[Any]) : Any = 
+		l match {
+			case head::Nil =>  head
+			case head::tails =>  last(tails)
+			case _ => Nil
+		}
+	
 
+	def main(args: Array[String]): Unit = {
+		println(last(List("1", 2, "3", '4')));	  
+	}
 }
--- src/test/scala/P01Spec.scala
+++ src/test/scala/P01Spec.scala
@@ -2,9 +2,22 @@ package org.p99.scala
 
 import org.scalatest._
 
-class P01Spec extends UnitSpec {
+class P01Spec extends FlatSpec {
 
-  // TODO
+	"last" should "return the head of 5xint list " in {
+    	val myList = List(12, 24, 36, 48, 50)
+		assert(P01.last(myList) == 50)
+  	}
+
+	"last" should "return the head of empty list " in {
+    	val myList = List()
+		assert(P01.last(myList) == Nil)
+  	}
+
+	"last" should "return the head of unique item list " in {
+    	val myList = List("azerty")
+		assert(P01.last(myList) == "azerty");
+  	}
 
 }
 
