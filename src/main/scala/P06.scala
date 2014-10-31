65966b1e6920b478e5b25f5d639ef5775c720b88
tsalmon <th_s@hotmail.fr>
2014-10-31 15:05:41 +0100

    p6 : add test and pre-cond

--- src/main/scala/P06.scala
+++ src/main/scala/P06.scala
@@ -3,6 +3,9 @@ package org.p99.scala
 object P06 {
 
   def isPalindrome(list:List[Any]) : Boolean = {
+    if(list.size == 0){
+      return true
+    }
     for(i <- 0 to list.size/2){
       if(list(i) != list(list.size - 1 - i)){
         return false
--- src/test/scala/P06Spec.scala
+++ src/test/scala/P06Spec.scala
@@ -8,4 +8,21 @@ class P06Spec extends UnitSpec {
       val myList = List(1, 2, 3, 2, 1)
       assert(P06.isPalindrome(myList) == true)
   }
+
+  "isPalindrome" should "true of List(1)" in {
+    val myList = List(1)
+    assert(P06.isPalindrome(myList) == true)
+  }
+
+  "isPalindrome" should "true of List()" in {
+    val myList = List()
+    assert(P06.isPalindrome(myList) == true)
+  }
+
+  "isPalindrome" should "false of List(1, 2, 3, 5, 2, 1)" in {
+    val myList = List(1, 2, 3, 5, 2, 1)
+    assert(P06.isPalindrome(myList) == false)
+  }
+
+
 }
