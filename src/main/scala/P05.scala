27d8d40b31cb7c32e1e277be85b22b55aec45234
tsalmon <th_s@hotmail.fr>
2014-10-31 14:43:26 +0100

    p5

--- src/main/scala/P05.scala
+++ src/main/scala/P05.scala
@@ -1,7 +1,9 @@
 package org.p99.scala
 
 object P05 {
-
-  // TODO
-
+  def reverse(list: List[Any]): List[Any] =
+    list match {
+        case Nil => List()
+        case head::tail => reverse(tail) ++ List(head)
+    }
 }
--- src/test/scala/P05Spec.scala
+++ src/test/scala/P05Spec.scala
@@ -4,7 +4,18 @@ import org.scalatest._
 
 class P05Spec extends UnitSpec {
 
-  // TODO
+  "reverse" should "return (5,4,3,2,1) of list (1,2,3,4,5)"  in {
+    val myList = List(1,2,3,4,5)
+    val retList = P05.reverse(myList)
+    for(i <- 0 to retList.size - 1){
+      assert(retList(i) == myList(retList.size - 1 - i))
+    }
+  }
 
+  "reverse" should "return List() of empty list"  in {
+    val myList = List()
+    val retList = P05.reverse(myList)
+    assert(retList.size == 0)
+  }
+  
 }
-
