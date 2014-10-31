fd596499254c639b8fed4959861690afea70d326
tsalmon <th_s@hotmail.fr>
2014-10-31 14:19:27 +0100

    p3

--- src/main/scala/P03.scala
+++ src/main/scala/P03.scala
@@ -1,7 +1,17 @@
 package org.p99.scala
 
 object P03 {
-
-  // TODO
-
+  def nth(l: List[Any], k: Int) : Any = {
+    if(k == 0){
+      l match {
+        case Nil => Nil
+        case head::tail => head
+      }
+    } else {
+        l match {
+          case Nil => throw new IndexOutOfBoundsException("Exception thrown")
+          case head::tail => nth(tail, k-1)
+        }
+    }
+  }
 }
--- src/test/scala/P03Spec.scala
+++ src/test/scala/P03Spec.scala
@@ -4,7 +4,16 @@ import org.scalatest._
 
 class P03Spec extends UnitSpec {
 
-  // TODO
+  "nth" should "return the kth element of list Any " in {
+    val myList = List(12, 24, 36, 48, 50)
+    assert(P03.nth(myList, 2) == 36)
+  }
+
+  "nth" should "return throw exception to get 2nd element of empty list" in {
+    val myList = List()
+    intercept[IndexOutOfBoundsException] {
+      P03.nth(myList, 2)
+    }
+  }
 
 }
-
