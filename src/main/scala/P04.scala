edd268c9b2a278c48298bf10cb90dc9a0ce1eafd
tsalmon <th_s@hotmail.fr>
2014-10-31 14:27:52 +0100

    p4

--- src/main/scala/P04.scala
+++ src/main/scala/P04.scala
@@ -1,7 +1,9 @@
 package org.p99.scala
 
 object P04 {
-
-  // TODO
-
+  def length(list: List[Any]): Int =
+    list match {
+      case Nil => 0
+      case head::tail => 1 + length(tail)
+    }
 }
--- src/test/scala/P04Spec.scala
+++ src/test/scala/P04Spec.scala
@@ -4,7 +4,23 @@ import org.scalatest._
 
 class P04Spec extends UnitSpec {
 
-  // TODO
+  "length" should "return 5 of a list of 5 elements" in {
+    val myList = List(1, 2, 4, 6, 6)
+    assert(P04.length(myList) == 5);
+  }
+
+  "length" should "return 0 of an empty list" in {
+    val myList = List()
+    assert(P04.length(myList) == 0);
+  }
+
+  "length" should "return 5 of a list 5 of list 5 int" in {
+    val myList = List(List(1, 2, 4, 6, 6),
+                      List(1, 2, 4, 6, 6),
+                      List(1, 2, 4, 6, 6),
+                      List(1, 2, 4, 6, 6),
+                      List(1, 2, 4, 6, 6))
+    assert(P04.length(myList) == 5);
+  }
 
 }
-
